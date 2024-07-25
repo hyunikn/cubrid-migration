70459cca60d1b88aad6fc0101e7da2a985f83278
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2024-07-25 12:50:43 +0900

    [TOOLS-4662] Packaging JRE 1.8 to run CMT in Java 1.7 or lower environment (#242)
    http://jira.cubrid.org/browse/TOOLS-4662

--- com.cubrid.cubridmigration.console/migration.bat
+++ com.cubrid.cubridmigration.console/migration.bat
@@ -1,4 +1,8 @@
 @echo off
-SET fn=%1 %2 %3 %4 %5 %6 %7 %8 %9
-java -Xms1024M -Xmx4096M -jar com.cubrid.cubridmigration.command-1.0.0-SNAPSHOT.jar %fn%
+set DIR=%CD%
+set JRE=%DIR%\jre
+set JAVA=%JRE%\bin\java
+set ARGS=%*
+
+%JAVA% -cp %JRE%\lib\*;%DIR%\lib\* -Xms1024M -Xmx4096M -jar %DIR%\com.cubrid.cubridmigration.command-1.0.0-SNAPSHOT.jar %ARGS%
 @echo on
--- com.cubrid.cubridmigration.console/migration.sh
+++ com.cubrid.cubridmigration.console/migration.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 DIR=$PWD
-java -jar -Xms1024M -Xmx4096M $DIR/com.cubrid.cubridmigration.command-1.0.0-SNAPSHOT.jar "$@"
+JRE=$DIR/jre
+JAVA=$JRE/bin/java
+
+$JAVA -cp $JRE/lib/*:$DIR/lib/* -Xms1024M -Xmx4096M -jar $DIR/com.cubrid.cubridmigration.command-1.0.0-SNAPSHOT.jar "$@"
